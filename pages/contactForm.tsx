7671d1fc1c063e108d18f70f84995f6ef0c1619e
Jailene <jaileneveras8@gmail.com>
2023-05-08 19:04:25 -0400

    created function for change in text area

--- pages/contactForm.tsx
+++ pages/contactForm.tsx
@@ -9,12 +9,17 @@ function ContactForm() {
     message: ''
   });
 
-  const handleInputChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+  const handleInputChange = (event: React.ChangeEvent<HTMLInputElement | HTMLTextAreaElement>) => {
     const { name, value } = event.target;
     setFormData({ ...formData, [name]: value });
   };
 
-  const handleSubmit = (event: React.FormEvent<HTMLFormElement>) => {
+  const handleTextareaChange = (event: React.ChangeEvent<HTMLTextAreaElement>) => {
+    const { name, value } = event.target;
+    setFormData({ ...formData, [name]: value });
+  };
+
+  const handleSubmit = (event) => {
     event.preventDefault();
 
     // Send email using EmailJS
@@ -43,7 +48,7 @@ function ContactForm() {
       </div>
       <div>
         <label htmlFor="message">Message:</label>
-        <textarea id="message" name="message" value={formData.message} onChange={handleInputChange} required></textarea>
+        <textarea id="message" name="message" value={formData.message} onChange={handleTextareaChange} required></textarea>
       </div>
       <button type="submit" className="text-gray-900 bg-gradient-to-r from-red-200 via-red-300 to-yellow-200 hover:bg-gradient-to-bl focus:ring-4 focus:outline-none focus:ring-red-100 dark:focus:ring-red-400 font-medium rounded-lg text-sm px-5 py-2.5 text-center mr-2 mb-2">Send message</button>
     </form>
